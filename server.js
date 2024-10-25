2cddb5d651ffc0424113a2f267e4e23eb2275496
Morgado18 <guiandrademelo108@gmail.com>
2024-10-25 19:53:54 +0100

    ok

--- server.js
+++ server.js
@@ -3,17 +3,17 @@ import app from "./src/app.js"
 
 const PORT = 3001
 
-import conexao from "./infra/conexao.js"
+/* import conexao from "./infra/conexao.js" */
 
-conexao.connect((error)=>{
+/* conexao.connect((error)=>{
     if(error){
         console.log(error)
     }else{
         console.log('Conexão realizada com sucesso!')
         // escutar a PORTa
-        app.listen(PORT, function(){
-            console.log(`Server rodando no endereço: http://localhost:${PORT}`)
-        })
     }
-})
+}) */
  
+    app.listen(PORT, function(){
+        console.log(`Server rodando no endereço: http://localhost:${PORT}`)
+    })
--- src/app.js
+++ src/app.js
@@ -2,19 +2,22 @@
 
 
 import express from "express"; // chama o módulo express
+/* import conexao from "../infra/conexao"; */
 const app = express() // instancia o express do módulo express
 /* const port = 3000 // porta para rodar o projecto */
 
 /* import conexao from "../infra/conexao.js"; */
 
+import conexao from "../infra/conexao.js";
+
 app.use(express.json()) // para o express ler o body de method post se houver
 
-const selecoes= [
+/* const selecoes= [
     {id: 1, selecao: "Angola", grupo: "G"},
     {id: 2, selecao: "Níger", grupo: "G"},
     {id: 3, selecao: "Brasil", grupo: "G"},
     {id: 4, selecao: "Sérvia", grupo: "G"},
-]
+] */
 
 function getSelecao(id){ 
     return selecoes.filter(selecao => selecao.id == id) //pega todos os dados
@@ -25,22 +28,59 @@ function getIndexSelecao(id){
 }
 
 // Rotas
-app.get("/", function(req, res){ // requisicao e uma resposta
+/* app.get("/", function(req, res){ // requisicao e uma resposta
     res.send("Hello World, I am leraning Node.js!")
-})
+}) */
 
 app.get("/selecoes", function(req, res){
-    res.status(200).send(selecoes)
+    //res.status(200).send(selecoes)
+    const sql = "SELECT * FROM selecoes"
+    conexao.query(sql, (erro, selecoes) => {
+        if(erro){
+            console.log(erro)
+        }else{
+            res.status(200).send(selecoes)
+        }
+    })
 })  
 
 app.get("/selecoes/:id", function(req, res){
-    let id_selecao = req.params.id
-    res.status(200).json(getSelecao(id_selecao))
+    const id_selecao = req.params.id
+    const sql = `SELECT * FROM selecoes WHERE id=?`
+   
+
+    conexao.query(sql, id_selecao, (erro, selecao_returned) => {
+        if(erro){
+         console.log(`Erro`+erro)
+        }else if (selecao_returned && selecao_returned.length > 0) {
+            const selecao = selecao_returned[0];
+            res.status(200).send(selecao); // Envia o resultado da seleção
+        } else {
+            res.status(404).send("Nenhum resultado encontrado"); // Caso não haja resultados
+        }
+     })
+
+
+    //res.status(200).json(getSelecao(id_selecao))
 })  
 
 app.post("/selecoes", (req, res) => {
-    selecoes.push(req.body) // pegar apenas os dados do body enviado pelo user
-    res.status(201).send("Added with success!")
+    //selecoes.push(req.body) // pegar apenas os dados do body enviado pelo user
+    //res.status(201).send("Added with success!")
+    const dados = req.body
+    const sql = "INSERT INTO selecoes SET ?"
+  //  res.send("ok")
+  
+  /* console.log("Dados recebidos:", req.body);  */
+    conexao.query(sql, dados, (erro, resultado) => {
+        if(erro){
+            console.log(`Erro`+erro)
+        }else{
+            res.status(201).json(resultado)
+        }
+    })
+
+    //console.log("Dados recebidos:", req.body); 
 })
 
 app.put("/selecoes/:id", function(req, res){
