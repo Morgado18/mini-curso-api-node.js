1d4c994870abc02cb278a8003f4f4ad632ac3f55
Morgado18 <guiandrademelo108@gmail.com>
2024-10-31 18:11:11 +0100

    ok

--- src/app/controllers/SelecaoController.js
+++ src/app/controllers/SelecaoController.js
@@ -1,37 +1,14 @@
 
-/* import conexao from "../database/conexao.js"
- */
-
 import SelecaoRepository from "../repositories/SelecaoRepository.js"
 
 class SelecaoController{
 
     async index(req, res){ //para aguardar a resposta "função async" e usar o await
-/*         const sql = "SELECT * FROM selecoes ORDER BY id DESC"
-        conexao.query(sql, (erro, selecoes) => {
-            if(erro){
-                console.log(erro)
-            }else{
-                res.status(200).send(selecoes)
-            }
-        }) */
        const data = await SelecaoRepository.findAll();
        res.json(data)
     }
 
     async show(req, res){
-        /* const id_selecao = req.params.id
-        const sql = `SELECT * FROM selecoes WHERE id=?`
-        conexao.query(sql, id_selecao, (erro, selecao_returned) => {
-            if(erro){
-             console.log(`Erro`+erro)
-            }else if (selecao_returned && selecao_returned.length > 0) {
-                const selecao = selecao_returned[0];
-                res.status(200).send(selecao); // Envia o resultado da seleção
-            } else {
-                res.status(404).send("Nenhum resultado encontrado"); // Caso não haja resultados
-            }
-         }) */
         const id = req.params.id
         const data = await SelecaoRepository.findById(id)
         res.json(data)
