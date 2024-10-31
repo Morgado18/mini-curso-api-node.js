5ecf336453aae34dd2bb2c35ebf91cdabed47dad
Morgado18 <guiandrademelo108@gmail.com>
2024-10-31 18:08:09 +0100

    ok

--- src/app/controllers/SelecaoController.js
+++ src/app/controllers/SelecaoController.js
@@ -6,21 +6,21 @@ import SelecaoRepository from "../repositories/SelecaoRepository.js"
 
 class SelecaoController{
 
-    index(req, res){
-        const sql = "SELECT * FROM selecoes ORDER BY id DESC"
+    async index(req, res){ //para aguardar a resposta "função async" e usar o await
+/*         const sql = "SELECT * FROM selecoes ORDER BY id DESC"
         conexao.query(sql, (erro, selecoes) => {
             if(erro){
                 console.log(erro)
             }else{
                 res.status(200).send(selecoes)
             }
-        })
-      /*  const data = SelecaoRepository.findAll();
-       res.json(data) */
+        }) */
+       const data = await SelecaoRepository.findAll();
+       res.json(data)
     }
 
-    show(req, res){
-        const id_selecao = req.params.id
+    async show(req, res){
+        /* const id_selecao = req.params.id
         const sql = `SELECT * FROM selecoes WHERE id=?`
         conexao.query(sql, id_selecao, (erro, selecao_returned) => {
             if(erro){
@@ -31,44 +31,30 @@ class SelecaoController{
             } else {
                 res.status(404).send("Nenhum resultado encontrado"); // Caso não haja resultados
             }
-         })
+         }) */
+        const id = req.params.id
+        const data = await SelecaoRepository.findById(id)
+        res.json(data)
     }
 
-    store(req, res){
-        const dados = req.body
-        const sql = "INSERT INTO selecoes SET ?"
-        conexao.query(sql, dados, (erro, resultado) => {
-            if(erro){
-                console.log(`Erro`+erro)
-            }else{
-                res.status(201).json(resultado)
-            }
-        })
+    async store(req, res){
+        const dados = req.body;
+        const data = await SelecaoRepository.create(dados);
+        res.json(data);
     }
 
-    update(req, res){
+    async update(req, res){
         const id_selecao = req.params.id
         const dados = req.body
-        const sql = "UPDATE selecoes SET ? WHERE id=?"
-        conexao.query(sql, [dados, id_selecao], (erro, resultado) =>{
-            if(erro){
-                console.log(`Erro`+erro)
-            }else{
-                res.status(200).send(`Updated! ${id_selecao}`)
-            }
-        })
+        const data = await SelecaoRepository.update(dados, id_selecao)
+        res.send(data)
+
     }
 
-    delete(req, res){
-        const id_selecao = req.params.id
-        const sql = "DELETE FROM selecoes WHERE id=?"
-        conexao.query(sql, id_selecao, (erro, resultado) =>{
-            if(erro){
-                console.log(`Erro`+erro)
-            }else{
-                res.status(200).send(`Deleted! ${id_selecao}`)
-            }
-        })
+    async delete(req, res){
+        const id = req.params.id
+        const selecao =  await SelecaoRepository.delete((id))
+        res.send(selecao)
     }
 
 }
--- src/app/repositories/SelecaoRepository.js
+++ src/app/repositories/SelecaoRepository.js
@@ -3,31 +3,82 @@ import conexao from "../database/conexao.js"
 
 class SelecaoRepository{
 
-    create(){
-
-    }
-
-    findAll(){
-        const sql = "SELECT * FROM selecoes ORDER BY id DESC"
-        conexao.query(sql, (erro, selecoes) => {
-            if(erro){
-                console.log(erro)
-            }else{
-                return selecoes
-            }
+    create(selecao){
+        const sql = "INSERT INTO selecoes SET ?"
+        return new Promise((resolve, reject)=>{
+            conexao.query(sql, selecao, (erro, resultado) => {
+                if(erro){
+                    return reject(`Erro`+erro)
+                }else{
+                    const resultado_formated = JSON.parse(JSON.stringify(resultado));
+                    return resolve(resultado_formated)
+                }
+            })
         })
     }
 
-    findById(){
-
+    findAll(selecao){
+        const sql = "SELECT * FROM selecoes ORDER BY id DESC"
+        return new Promise((resolve, reject) => { 
+            conexao.query(sql, (erro, selecoes) => {
+                if(erro){
+                    console.log(erro)
+                }else{
+                    const dados = JSON.parse(JSON.stringify(selecoes)) // converte em string/objcto
+                    return resolve(dados)
+                }
+            })
+        })
     }
 
-    update(){
-
+    findById(id){
+        const sql = `SELECT * FROM selecoes WHERE id=?`
+        return new Promise((resolve, reject)=>{
+            conexao.query(sql, id, (erro, selecao_returned) => {
+                if(erro){
+                 console.log(`Erro`+erro)
+                }else if (selecao_returned && selecao_returned.length > 0) {
+                    const selecao_converted = JSON.parse(JSON.stringify(selecao_returned));
+                    return resolve(selecao_converted); // Envia o resultado da seleção
+                } else {
+                    return reject("Nenhum resultado encontrado"); // Caso não haja resultados
+                }
+             })
+        })
+        /* conexao.query(sql, id_selecao, (erro, selecao_returned) => {
+            if(erro){
+             console.log(`Erro`+erro)
+            }else if (selecao_returned && selecao_returned.length > 0) {
+                const selecao = selecao_returned[0];
+                res.status(200).send(selecao); // Envia o resultado da seleção
+            } else {
+                res.status(404).send("Nenhum resultado encontrado"); // Caso não haja resultados
+            }
+         }) */
     }
 
-    delete(){
+    update(dados, id_selecao){
+        const sql = "UPDATE selecoes SET ? WHERE id=?"
+        return new Promise((resolve, reject)=>{
+            conexao.query(sql, [dados, id_selecao], (erro, resultado) =>{
+                if(erro){
+                    return reject(`Erro`+erro)
+                }else{
+                    return resolve(`Updated! ${id_selecao}`)
+                }
+            })
+        })
+    }
 
+    delete(id){
+        const sql = "DELETE FROM selecoes WHERE id=?"
+        return new Promise((resolve, reject)=>{
+            conexao.query(sql, id, (erro, selecao)=>{
+                if(erro) {return reject(`Houve um erro...`)}else{
+                    return resolve('Eliminado com sucesso!')
+                }
+            })
+        })
     }
 
 }
